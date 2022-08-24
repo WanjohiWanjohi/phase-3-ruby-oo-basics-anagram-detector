806367cfade4def097b431f9745b99da417c8c3b
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-08-24 21:45:29 +0300

    commit tree 7f91eb26c51c488fa5e9d28e0bbeb2c4c0c8d8bc

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(words)
+        matched_words = []
+        words.each do |word|
+            if word.chars.sort() == @word.chars.sort()
+                matched_words << word
+            end
+        end
+        return matched_words
+    end
+end
